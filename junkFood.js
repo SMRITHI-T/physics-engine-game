3f1dfc49576441e414fdda27120813dc25fac627
SMRITHI-T <74653719+SMRITHI-T@users.noreply.github.com>
2021-05-10 18:05:27 +0530

    Add files via upload

--- junkFood.js
+++ junkFood.js
@@ -1,32 +1,27 @@
-class Player {
-    constructor(x,y,radius){
+class JunkFood {
+    constructor(x,y,radius,img){
         var option ={
             density:1,
             friction:0.5,
-    isStatic:true 
+            isStatic:true 
         }
         this.x=x;
         this.y=y;
         this.radius=radius;
         this.body=Bodies.circle(x,y,radius/2,option)
-        this.animation=loadAnimation("images/player1.png","images/player2.png","images/player3.png")
+        this.image=img
         World.add(world,this.body);
 
     }
     display(){
+       
         var pos=this.body.position;
         var angle=this.body.angle;
         push()
         translate(pos.x,pos.y)
         rotate(angle)
         ellipseMode (CENTER)
-        animation(this.animation,0,0,this.radius,this.radius);
+        image(this.image,0,0,this.radius,this.radius);
         pop()
-    }
-    move(a,b){
-        this.body.position.x=this.body.position.x+a 
-        this.body.position.y=this.body.position.y+b
-
-
     }
 }
--- sketch.js
+++ sketch.js
@@ -5,8 +5,13 @@ var engine,world;
 var player;
 var brinjal,burger,capsicum,donut,hotdog,onion,pizza,potato;
 var brinjalImg,burgerImg,capsicumImg,donutImg,hotdogImg,onionImg,pizzaImg,potatoImg;
-var healthyGroup,junkGroup;
-var health=0;
+var health = 100;
+var junkGroup
+var healthyFood= []
+var junkFood=[];
+var img
+var gameState=0;
+
 function preload(){
 backgroundImg=loadImage("images/background.jpg")
 brinjalImg=loadImage("images/brinjal.png")
@@ -17,7 +22,8 @@ hotdogImg=loadImage("images/hotdog.png")
 onionImg=loadImage("images/onion.png")
 pizzaImg=loadImage("images/pizza.png")
 potatoImg=loadImage("images/potato2.png")
-heartImg=loadImage("images/heart.png")
+sickBoy=loadImage("images/sick boy.png")
+healthyBoy=loadImage("images/healthyboy.png")
 
 
 }
@@ -26,109 +32,192 @@ function setup() {
   createCanvas(1320,600);
   engine=Engine.create()
   world=engine.world
-player=new Player(80,500,25);
-var heart1=createSprite(800,60,20,20);
-var heart2=createSprite(820,60,20,20);
-var heart3=createSprite(840,60,20,20);
-var heart4=createSprite(860,60,20,20);
-var heart5=createSprite(880,60,20,20);
-var heart6=createSprite(900,60,20,20);
-var heart7=createSprite(920,60,20,20);
-var heart8=createSprite(940,60,20,20);
-var heart9=createSprite(960,60,20,20);
-var heart10=createSprite(980,60,20,20);
-var heart11=createSprite(1000,60,20,20);
-var heart12=createSprite(1020,60,20,20);
-var heart13=createSprite(1040,60,20,20);
-var heart14=createSprite(1060,60,20,20);
-var heart15=createSprite(1080,60,20,20);
-heart1.visble=true;
-heart2.visble=true;
-heart3.visble=true;
-heart4.visble=false;
-heart6.visble=false;
-heart7.visble=false;
-heart8.visble=false;
-heart9.visble=false;
-heart10.visble=false;
-heart11.visble=false;
-heart12.visble=false;
-heart13.visble=false;
-heart14.visble=false;
-heart15.visble=false;
 
-bg=createSprite(850,150,1350,600)
-bg.scale=3.1;
-bg.addImage(backgroundImg);
-healthyGroup=new Group();
-junkGroup=new Group();
+  player=new Player(150,500,25);
+
+  bg=createSprite(850,150,1350,600)
+  bg.scale=3.1;
+  bg.addImage(backgroundImg);
+
+
+  junkGroup = new Group()
 }
+
 function draw() {
   Engine.update(engine)
+
   background("blue"); 
-   bg.velocityX=-5;
-   drawSprites();
-player.display();
-if(keyDown(UP_ARROW)){
-  player.move(0,-5)
-}
-if(keyDown(DOWN_ARROW)){
-  player.move(0,5)
-}
-if(bg.x<400){
-bg.x=bg.width/2;
-}
-text("LIVES: ", 790,60)
-text("goal=15",1200,50);
-if(player.isTouching(healthyGroup)){
-heart4.visible=true;
-}
-if(player.isTouching(junkGroup)){
-  heart4.visible=true;
+  drawSprites();
+  textSize(50);
+  fill("white")
+  text("Health: " + health, 200,50)
+
+  
+
+if(gameState===0){
+  bg.velocityX=-5;
+
+  player.display();
+
+  if(keyDown(UP_ARROW)&&player.body.position.y>90){
+    player.move(0,-8)
   }
-healthy();
-junk ();
-}
-function healthy(){
-  if(frameCount%200===0){
-var healthyFood=createSprite(1320,500,10,10);
-healthyFood.velocityX=-5;
-healthyFood.scale=0.15;
-healthyFood.y=Math.round(random(50,550))
-var img =Math.round(random(1,4))
-switch(img){
-  case 1:healthyFood.addImage(brinjalImg);
-  break;
-  case 2:healthyFood.addImage(capsicumImg);
-  break;
-  case 3:healthyFood.addImage(onionImg);
-  break;
-  case 4:healthyFood.addImage(potatoImg);
-  break;
-}
-healthyFood.setLifetime=270;
-healthyGroup.add(healthyFood);
+
+  if(keyDown(DOWN_ARROW)&&player.body.position.y<510){
+    player.move(0,8)
   }
-}
-function junk(){
-  if(frameCount%150===0){
-    var junkFood=createSprite(1320,500,10,10);
-    junkFood.velocityX=-5;
-    junkFood.scale=0.1;
-    junkFood.y=Math.round(random(50,550))
-    var img =Math.round(random(1,4))
-    switch(img){
-      case 1:junkFood.addImage(donutImg);
+
+  if(bg.x<400){
+    bg.x=bg.width/2;
+  }
+  if(health<=50){
+   
+gameState=1;
+  }
+  if(health>=200){
+   
+    gameState=2;
+  }
+
+  if(frameCount%75===0){
+    var rand = Math.round(random(1,4))
+    switch(rand){
+    case 1:img = brinjalImg
       break;
-      case 2:junkFood.addImage(hotdogImg);
+    case 2:img = capsicumImg
       break;
-      case 3:junkFood.addImage(pizzaImg);
+    case 3:img = onionImg
       break;
-      case 4:junkFood.addImage(burgerImg);
+    case 4:img = potatoImg
       break;
+    default: break;
     }
-    junkFood.setLifetime=270;
-    junkGroup.add(junkFood);
-      }
+  
+  healthyFood.push(new HealthyFood(1320, random(50,550), 50,img))
+ }
+ if(frameCount%50===0){
+  var rand = Math.round(random(1,4))
+  switch(rand){
+  case 1:img = pizzaImg
+    break;
+  case 2:img = hotdogImg
+    break;
+  case 3:img = donutImg
+    break;
+  case 4:img = burgerImg
+    break;
+  default: break;
+  }
+
+junkFood.push(new JunkFood(1320, random(50,550), 50,img))
 }
+
+  for(var i=0; i<healthyFood.length;i++){
+
+    
+if(healthyFood[i]!=null){
+  healthyFood[i].display();
+    healthyFood[i].body.position.x += -18
+    if(healthyFood[i].body.position.x <= -100){
+      healthyFood[i] = null;
+    }
+    if(detectollision(player,healthyFood[i])){
+        healthyFood[i]=null
+        health = health+ 10
+    }
+    
+}
+  }
+
+  for(var i=0; i<junkFood.length;i++){
+
+    
+    if(junkFood[i]!=null){
+      junkFood[i].display();
+        junkFood[i].body.position.x += -18
+        if(junkFood[i].body.position.x <= -100){
+          junkFood[i] = null;
+        }
+        if(detectollision(player,junkFood[i])){
+            junkFood[i]=null
+            health = health- 10
+        }
+        
+    }
+      }
+    }
+    if(gameState===1){
+      bg.velocityX=0;
+      textSize(50);
+      text("YOU'RE SICK...",500,300);
+      image(sickBoy,550,400,200,200);
+    }
+    if(gameState===2){
+      bg.velocityX=0;
+      textSize(50);
+      text("YOU'RE SO HEALTHY NOW!!!",350,300);
+      image(healthyBoy,550,370,200,200);
+    }
+
+}
+
+function detectollision(lplayer,healthJunk){
+  if(healthJunk!=null){
+
+  playerPosition=lplayer.body.position
+  healthJunkPosition=healthJunk.body.position
+  
+  var distance=dist(playerPosition.x, playerPosition.y, healthJunkPosition.x, healthJunkPosition.y)
+  if(distance<=player.radius+healthJunk.radius) {
+     return true
+    }
+  }
+
+  }
+
+
+// function healthy(){
+//   if(frameCount%200===0){
+// var healthyFood=createSprite(1320,500,10,10);
+// healthyFood.velocityX=-5;
+// healthyFood.scale = 0.1
+// healthyFood.y = Math.round(random(50,550))
+// var img =Math.round(random(1,4))
+// switch(img){
+//   case 1:healthyFood.addImage(brinjalImg);
+//   break;
+//   case 2:healthyFood.addImage(capsicumImg);
+//   break;
+//   case 3:healthyFood.addImage(onionImg);
+//   break;
+//   case 4:healthyFood.addImage(potatoImg);
+//   break;
+// }
+// healthyFood.setLifetime=270;
+// healthyGroup.add(healthyFood)
+//   }
+// }
+
+
+// //function junk(){
+//   //if(frameCount%350===0){
+//     //var junkFood=createSprite(1320,500,10,10);
+//     junkFood.velocityX=-5;
+//     junkFood.scale = 0.1
+//     junkFood.y = Math.round(random(50,550))
+//     var img =Math.round(random(1,4))
+//     switch(img){
+//       case 1:junkFood.addImage(burgerImg);
+//       break;
+//       case 2:junkFood.addImage(donutImg);
+//       break;
+//       case 3:junkFood.addImage(pizzaImg);
+//       break;
+//       case 4:junkFood.addImage(burgerImg);
+//       break;
+//     }
+//     junkFood.setLifetime=270;
+//     junkGroup.add(junkFood)
+//       }
+// }
 
